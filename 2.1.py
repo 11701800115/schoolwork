1b3147521c4323a23c05e7ba0a85feaf80c87a4e
11701800115 <43084677+11701800115@users.noreply.github.com>
2018-10-11 17:49:24 +0800

    Add files via upload

--- 2.1.py
+++ 2.1.py
@@ -1,10 +1,12 @@
 #e1.1TempConvert.py
-TempStr = eval(input("请输入带有符号的温度值: ")
-if TempStr[-1] in ['F','f']:
-               C= (TempSter-32)/1.8)
-               print ("转换后的温度是{:.2f}C".format (C))
-elif TempStr[-1] in ['c','C']:
-               F=1.8*TempStr+32
-               print("转换后的温度是{:.2f}F".format(F))
+SymbolStr = input("请输入温度体系符号: ")
+if SymbolStr in ['F', 'f']:
+    t = eval(input("请输入温度值: "))
+    C = (t - 32) / 1.8
+    print("转换后的温度是:{:.0f}C".format(C))
+elif SymbolStr in ['C', 'c']:
+    t = eval(input("请输入温度值: "))
+    F = t * 1.8 + 32
+    print("转换后的温度是:{:.0f}F".format(F))
 else:
-               print("输入格式错误“）
+    print("输入符号错误")
