3cf1839d9560ad81db3124ca6b0d47739e431c11
11701800115 <43084677+11701800115@users.noreply.github.com>
2018-10-12 08:30:18 +0800

    Add files via upload

--- 2.2.py
+++ 2.2.py
@@ -1,10 +1,9 @@
-TempStr = eval(input("请输入带有符号的金额: ")
-if  TempStr[-1] in ['$'] :
-    C= (eval(TempSter[0:-1])*6)
-    print ("转换后的金额是{：。2f}￥".format (￥))
-elif TempStr[-1] in ['￥']:
-     F=(eval(TempStr[0:-1])/6
-     print("转换后的金额是{：。2f}$".format($))
+TempStr = input("请输入带有符号的金额: ")
+if  TempStr[-1] in ["$"]:
+    C= eval(TempStr[0:-1])*6
+    print ("转换后的金额是{:.2f}￥".format (C))
+elif TempStr[-1] in ["￥"]:
+     F=eval(TempStr[0:-1])/6
+     print("转换后的金额是{:.2f}$".format(F))
 else:
-     print("输入格式错误“）
-
+     print("输入格式错误")
